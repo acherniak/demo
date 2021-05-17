104a59af173a49ac73aed4c8afa5599791c1be91
Alex Cherniak <alex.cherniak@vaporstream.com>
2021-05-17 06:54:17 -0400

    update

--- alex-mongo.js
+++ alex-mongo.js
@@ -1,22 +1,29 @@
-const express = require('express'),Chance = require('chance'), app = express(), port = 3702,
-	mongoose = require('mongoose');
+const express = require('express'),Chance = require('chance'), app = express(), port = 3701,
+	mongoose = require('mongoose'), got = require('got');//{ addAvatar } = require('./avatar');
 
 mongoose.connect('mongodb://127.0.0.1/alex', {useNewUrlParser: true, useUnifiedTopology: true});
 let mongo = mongoose.connection,
-	schema = new mongoose.Schema({ name: String, dob: Date,
-		custom: { email: String, phone: String, adr: String }
+	schema = new mongoose.Schema({ name: String, dob: Date, avatar: String,
+		custom: { email: String, phone: String, adr: String, city: String, state: String }
 	}),
 	person = new mongoose.model('person', schema);
 
+app.get('/info', async (req, res) => res.send({db: 'MongoDB'}));
 app.get('/staff', async (req, res) => person.find({}, (err,staff) => res.send(staff.map(p=>p._doc))));
 app.get('/staff/db', async (req, res) => res.send({ ver: 'MongoDB', 
-	staff: '{ name: String, dob: Date, custom: { email: String, phone: String, adr: String }}'
+	staff: '{ name: String, dob: Date, avatar: String, custom: { email: String, phone: String, adr: String }}'
 }))
 
-app.put('/add/:n', async (req, res) => { let chance = new Chance(), name, n=req.params.n, IDs = [];
-	for (let i=0; i<n; i++) IDs.push((await person.create({ name: name=chance.name(), dob: chance.birthday(),
-		custom: { email: name.split(' ').join('.').toLocaleLowerCase()+'@ac.com', adr: chance.address({short_suffix: true}), city: chance.city(), state: chance.state(), phone: chance.phone() }
-	}))._id.toString());
+app.put('/add/:n', async (req, res) => { let chance = new Chance(), n=req.params.n, IDs = [];
+	for (let i=0; i<n; i++) { let pers, id, gender = chance.gender().toLowerCase(), name = chance.name({gender:gender}), state;
+		pers = await person.create({ name: name, dob: chance.birthday(),
+			custom: { state: state=chance.state(), adr: chance.address({short_suffix: true}), city: chance.city(),
+			phone: chance.phone(), email: `${name.split(' ').join('.').toLocaleLowerCase()}@${state.toLowerCase()}.us.net` }
+		});
+		IDs.push(id = pers._id.toString());
+		(async(pers, gender, id) => { pers.avatar = (await got(`https://avatars.dicebear.com/api/${gender}/${id}.svg`)).body; await pers.save();
+		})(pers, gender, id);
+	}
 	res.send(IDs)
 });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { format } from 'date-fns'
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort'
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSidenav } from '@angular/material/sidenav';
 import { ApiService } from './api.service';
+import { DomSanitizer, Title } from '@angular/platform-browser';
 
 @Component ({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: ['.mat-paginator {background-color:transparent;}']
+  styles: ['.mat-paginator {background-color:transparent;}', '.mat-column-avatar {width:40px;}',
+		'.svg { width:200px; margin:10% auto; } .svg:hover { width:500px }']
 })
 export class AppComponent implements OnInit{
-  title = "Alex's Staff"; list = new MatTableDataSource<any>([]); displCols =['_id','name','dob','email','phone','adr','city','state'];
-	curRow: any = {};
+  title = "Alex's Staff"; list = new MatTableDataSource<any>([]); cols = ['_id','name','dob','email','phone','adr','city','state'];
+	displCols = ['avatar', ...this.cols]; curRow: any = {};
 	@ViewChild(MatPaginator) paginator!: MatPaginator; @ViewChild(MatSort) sort!: MatSort;
 	@ViewChild(MatSidenav) sidenav!: MatSidenav;
-	constructor(public api: ApiService) {}
-	ngOnInit(): void {
-		this.init()
+	
+	constructor(public api: ApiService, private sanitizer: DomSanitizer, private titleService: Title) {}
+	ngOnInit(): void { this.init()
+		this.api.getHttp('info').subscribe((res:any) => this.titleService.setTitle(res.db));
 	}
 	show = (rows:any[]) => {
 		this.list = new MatTableDataSource(rows);
@@ -28,12 +31,16 @@ export class AppComponent implements OnInit{
 	init = () => this.api.getHttp('staff').subscribe((res: any) =>
 		this.show(res.map((el:any)=>{ let cust = el.custom;
 			return { _id:el._id, name:el.name,dob:!el.dob?'':format(new Date(el.dob),'yyyy-MM-dd'),
-				email:cust.email,phone:cust.phone,adr:!cust.adr? '':cust.adr+(!cust.city?'':', '+cust.city)+(!cust.state?'':' '+cust.state)}
+				avatar: !el.avatar?undefined:this.sanitizer.bypassSecurityTrustHtml(el.avatar),
+				email:cust.email,phone:cust.phone,adr:cust.adr,city:cust.city,state:cust.state}
 		})))
-	selRow = (row:any) => { this.curRow = row; this.sidenav.open(); }
-	add = (n:number) => this.api.putHttp('add/'+n, {}).subscribe(res => this.init());
-	clear = () => this.api.deleteHttp('clear').subscribe(res => this.init());
-	delete = (id:number) => this.api.deleteHttp('delete/'+id).subscribe(res => {
+	selRow = (row:any) => {
+		this.curRow = row; this.sidenav.open();
+		console.log(row.avatar);
+	}
+	add = (n:number) => this.api.putHttp('add/'+n, {}).subscribe((res:any) => this.init());
+	clear = () => this.api.deleteHttp('clear').subscribe((res:any) => this.init());
+	delete = (id:number) => this.api.deleteHttp('delete/'+id).subscribe((res:any) => {
 		let list = this.list.data, n = list.findIndex(row=>row._id==id);
 		if (n>=0) { list.splice(n, 1); this.show(list); this.sidenav.close(); }
 	});
